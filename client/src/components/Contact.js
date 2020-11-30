843e06e4fca94b0b8e7b053dee84c137d321d7ac
Patrick Heflin <patrickslx@yahoo.com>
2020-11-30 10:55:25 -0600

    change title

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,7 +4,6 @@ function Contact() {
   return (
     <div className='contact-container'>
       <div className='contact-text'>
-        <h1>CONTACT</h1>
         <p>Feel free to contact me anytime!</p>
         <a href="mailto: patrickslx@yahoo.com">patrickslx@yahoo.com</a>
       </div>
